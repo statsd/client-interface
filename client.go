17248482b1fcc7b4773122b6667ee22066e6db3d
TJ Holowaychuk <tj@vision-media.ca>
2014-08-07 15:38:36 -0700

    Initial commit

---
+++ client.go
@@ -0,0 +1,18 @@
+package statsd
+
+import "time"
+
+type Client interface {
+	Prefix(string)
+
+	Gauge(name string, value int) error
+
+	Incr(name string) error
+	IncrBy(name string, count int) error
+
+	Decr(name string) error
+	DecrBy(name string, count int) error
+
+	Duration(name string, duration time.Duration) error
+	Histogram(name string, value int) error
+}
